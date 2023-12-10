5e6ee2906e050d24d04bdddd369740ec15578930
Katy <wc-katy@yandex.ru>
2023-12-10 15:52:58 +0700

    task 5 added

--- test-code.py
+++ test-code.py
@@ -1,2 +1,12 @@
 name = input('what is your name: ')
 print(f'hi, {name}! how are you?')
+
+
+"""
+Задача 5
+Дано число. Проверить, кратно ли оно 5 и 10 или 15 но не 30
+"""
+
+num = int(input('Введите число: '))
+
+print((num % 5 == 0 and num % 10 == 0) or (num % 15 == 0 and num % 30 != 0))
